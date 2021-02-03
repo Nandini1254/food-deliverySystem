9ef4b9567c2dfb8714a17aac91755e20ffcc4157
Nandini <nandinipanchani@gmail.com>
2021-02-04 02:56:18 +0530

    login module

--- Mainfolder/Mainfolder/urls.py
+++ Mainfolder/Mainfolder/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf.urls import url
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
     path('OrderingOnline/',include('OrderingOnline.urls')),
+    path('loginmodule/',include('loginmodule.urls')),
 ]
---
+++ Mainfolder/loginmodule/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.models import auth,User
+# # Create your views here.
+def auth_view(request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)   
+            return HttpResponseRedirect('/loginmodule/loggedin/')
+        else:
+            return HttpResponseRedirect('/loginmodule/login/')
+def login_view(request):
+    return render(request,'login.html')
+def loggedin(request):
+    return render(request,'loggedin.html',{"fullname":"request.user.username"})
+def logout_view(request):
+    auth.logout(request)
+    return render(request,'logout.html')
+def invalidlogin(request):
+    return render(request,'invalidlogin.html')
